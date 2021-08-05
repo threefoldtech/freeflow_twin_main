9f315e46423b2b4cbeb17e366facd53c91253b2e
Arno Baert <arnobaert@Arnos-MacBook-Pro.local>
2021-08-05 10:23:12 +0200

    Userdropdown changed into table when inviting for chat

---
+++ src/components/UserTable.vue
@@ -0,0 +1,154 @@
+<template>
+    <div>
+        <label class="mr-2" for="username">Username:</label>
+        <input
+            v-model="searchTerm"
+            @focus="handleInput"
+            @input="handleInput"
+            :placeholder="placeholder"
+            v-focus
+            tabindex="0"
+            maxlength="50"
+        />
+    </div>
+    <span class="text-red-600" v-if="error != ''"> {{ error }} </span>
+    <span
+        v-if="modelValue"
+        @click.prevent="reset()"
+        class="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
+    >
+        x
+    </span>
+    <div class="flex flex-col mt-4">
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+          <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+              <tr>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Users
+                </th>
+              </tr>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-200">
+              <tr v-for="(item, index) in searchResults()" :key="index">
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="flex items-center justify-between">
+                    <div class='flex'>
+                    <div class="flex-shrink-0 h-10 w-10">
+                      <AvatarImg :id="item.id" alt="contact image" />
+                    </div>
+                    <div class="ml-4">
+                      <div class="text-sm font-medium text-gray-900">
+                        {{ item.id }}
+                      </div>
+                      <div class="text-sm text-gray-500">
+                        {{ item.location }} 205546654
+                      </div>
+                    </div>
+                    </div>
+                    <button @click="handleClick(item)" style="backgroundColor: #16A085;" class="text-white rounded-md justify-self-end">Invite to chat</button>
+                  </div>
+                </td>
+                <td v-if="!searchResults().length"><p>No Matching Results</p></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+    import { Contact } from '@/types';
+    import { defineComponent, ref, computed, onMounted } from 'vue';
+    import AvatarImg from '@/components/AvatarImg.vue';
+    export default defineComponent({
+        components: { AvatarImg },
+        props: {
+            modelValue: {
+                type: String,
+                required: false,
+            },
+            placeholder: {
+                type: String,
+                required: false,
+                default: 'Enter text here.',
+            },
+            data: {
+                type: Array,
+                required: true,
+            },
+            dropdownClass: {
+                type: String,
+                required: false,
+                default:
+                    'absolute w-full z-50 bg-white border border-gray-300 mt-1 mh-48 overflow-hidden overflow-y-scroll rounded-md shadow-md',
+            },
+            error: {
+                type: String,
+                default: '',
+            },
+        },
+        emits: ['update:modelValue', 'clicked'],
+
+        setup(props, { emit }) {
+            const showOptions = ref(false);
+            const chosenOption = ref('');
+            const searchTerm = ref('');
+
+            const reset = () => {
+                emit('update:modelValue', '');
+                chosenOption.value = '';
+                searchTerm.value = '';
+            };
+
+            const handleInput = evt => {
+                emit('update:modelValue', evt.target.value);
+                showOptions.value = true;
+            };
+
+            const handleClick = item => {
+                console.log(chosenOption.value);
+                chosenOption.value = item.id;
+                searchTerm.value = item.id;
+                emit('update:modelValue', item.id);
+                showOptions.value = false;
+                emit('clicked');
+            };
+
+            const clickedOutside = () => {
+                showOptions.value = false;
+
+                if (!chosenOption.value) {
+                    emit('update:modelValue', '');
+                }
+            };
+            const searchResults = () => {
+                return props.data.filter((item: Contact) => {
+                    return item.id
+                        .toLowerCase()
+                        .includes(searchTerm.value.toLowerCase());
+                });
+            };
+
+            return {
+                reset,
+                handleInput,
+                handleClick,
+                clickedOutside,
+                showOptions,
+                chosenOption,
+                searchTerm,
+                searchResults,
+            };
+        },
+    });
+</script>
+
+<style scoped>
+    .mh-48 {
+        max-height: 10rem;
+    }
+</style>
