fb47f1f19f719574c4502c86d4f8fde2a686d020
Jonas Wijne <jonas.wijne@jimber.org>
2021-08-18 10:41:48 +0200

    WIP improvements

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,15 +1,15 @@
 <template>
     <nav>
-        <div class="flex flex-col h-full items-center mx-2">
-            <div class="mb-4 grid text-center text-white cursor-pointer">
+        <div class='flex flex-col h-full items-center mx-2'>
+            <div class='mb-4 grid text-center text-white cursor-pointer'>
                 <div
-                    class="h-20 w-20 rounded-full grid place-items-center mb-1"
-                    style="position: relative"
+                    class='h-20 w-20 rounded-full grid place-items-center mb-1'
+                    style='position: relative'
                     @click="changePage('chat')"
                 >
-                    <img src="../../public/TF-small.svg" width="50" alt="Threefold logo" />
+                    <img alt='Threefold logo' src='../../public/TF-small.svg' width='50' />
 
-                    <img src="../../public/digitaltwin.png" width="100" alt="Threefold logo" />
+                    <img alt='Threefold logo' src='../../public/digitaltwin.png' width='100' />
                     <!-- <div class="text-sm">
                         <strong>digital </strong>
                         <span>twin</span>
@@ -18,40 +18,39 @@
             </div>
             <br />
             <div
-                v-for="app in apps"
-                :key="app.name"
-                class="mb-4 grid text-center text-white"
+                v-for='app in apps'
+                :key='app.name'
                 :class="{
                     'cursor-pointer': app.enabled,
                     selected: app?.enabled && router.currentRoute?.value.meta.app !== app.name,
                     'text-gray-500': !app?.enabled,
                 }"
+                class='mb-4 grid text-center text-white'
             >
                 <div
-                    class="h-20 w-20 rounded-full grid place-items-center mb-1"
-                    style="position: relative"
-                    @click="changePage(app.name)"
+                    class='h-20 w-20 rounded-full grid place-items-center mb-1'
+                    style='position: relative'
+                    @click='changePage(app.name)'
                 >
-                    <i :class="`fas ${app.icon} text-2xl`"></i>
+                    <i :class='`fas ${app.icon} text-2xl`'></i>
                     <h3>
                         {{ app.name }}
                     </h3>
                 </div>
             </div>
-            <div class="grid place-items-end flex-grow">
-                <div
-                    class="w-20 h-20 grid cursor-pointer items-center justify-center justify-items-center"
-                    @click="toggleShowUserConfigDialog"
-                >
-                    <AvatarImg :id="user.id" />
-                    <!--<h3 class="truncate w-full text-sm">{{ user.id }}</h3>-->
-                </div>
+            <div class='flex-grow'></div>
+            <div
+                class='w-20 h-20 grid cursor-pointer items-center justify-center justify-items-center'
+                @click='toggleShowUserConfigDialog'
+            >
+                <AvatarImg :id='user.id' />
+                <!--<h3 class="truncate w-full text-sm">{{ user.id }}</h3>-->
             </div>
         </div>
     </nav>
 </template>
 
-<script lang="ts">
+<script lang='ts'>
     import { defineComponent, computed } from 'vue';
     import { useRouter } from 'vue-router';
     import AvatarImg from '@/components/AvatarImg.vue';
