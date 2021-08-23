d7914521ea0c217f917bc977de41d51a724bd672
Jonas Wijne <jonas.wijne@jimber.org>
2021-08-23 14:02:08 +0200

    ux improvement

--- src/components/MessageContentType/AudioContent.vue
+++ src/components/MessageContentType/AudioContent.vue
@@ -1,18 +1,22 @@
 <template>
-    <audio
-        controls
-        class="max-w-full"
-        :src="calcExternalResourceLink(message.body.url)"
-        @load="$emit('scroll')"
-    ></audio>
+    <div class='p-3 mt-2'>
+        <audio
+            controls
+            class='max-w-full'
+            :src='calcExternalResourceLink(message.body.url)'
+            @load="$emit('scroll')"
+        ></audio>
+    </div>
 </template>
 
 <script lang="ts">
     import { computed, defineComponent } from 'vue';
     import { calcExternalResourceLink } from '@/services/urlService';
+    import AudioPlayer from '@/components/AudioPlayer.vue';
 
     export default defineComponent({
         name: 'AudioContent',
+        components: { AudioPlayer },
         props: {
             message: { type: Object, required: true },
         },
