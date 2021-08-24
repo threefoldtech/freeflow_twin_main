8cfa4b8f0ad95da90f2ebc15879b606285cbebc1
Jonas Wijne <jonas.wijne@jimber.org>
2021-08-24 18:57:49 +0200

    debug info

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
-    <suspense>
-        <router-view />
-    </suspense>
+    <div
+        :class='{
+            "debug-screens": isDev
+        }'
+        class='h-screen'
+    >
+        <suspense>
+            <router-view />
+        </suspense>
+        <div v-if='isDev && location' class='fixed text-white bg-black -right-px -bottom-0.5 border border-white px-2 text-xs'>
+            {{ location }}
+        </div>
+    </div>
 </template>
 
-<script lang="ts" setup>
+<script lang='ts' setup>
     import version from '../public/config/version';
+    import { myYggdrasilAddress } from '@/store/authStore';
+    import { ref } from 'vue';
+
     console.log('Version: ' + version);
+
+    const isDev = import.meta.env.DEV;
+    const location = ref();
+    myYggdrasilAddress().then(v => location.value = v);
 </script>
 
 <style></style>
