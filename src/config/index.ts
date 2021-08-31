a1e053c28671308bc8a94b25dc16f598ee46cfa1
Jonas Wijne <jonas.wijne@jimber.org>
2021-08-31 11:14:04 +0200

    beta banner

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,12 @@
 interface IConfig {
-    baseUrl: string,
-    appBackend: string,
-    documentServerUrl: string,
-    giphyApiKey: string,
+    baseUrl: string;
+    appBackend: string;
+    documentServerUrl: string;
+    giphyApiKey: string;
+    beta: boolean | null;
 }
 
 //@ts-ignore
-const config = <IConfig>window.config
+const config = <IConfig>window.config;
 
 export default config;
