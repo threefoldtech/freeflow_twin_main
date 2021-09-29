032ed7af5ded6b815570be3229f0d6aa91bbb99b
GillesMommerency <gilles.mommerency@student.howest.be>
2021-09-29 14:21:46 +0200

    Fixed directory search issue

--- src/views/app/FileBrowser.vue
+++ src/views/app/FileBrowser.vue
@@ -13,39 +13,56 @@
     </appLayout>
 </template>
 
-<script lang="ts">
-    import appLayout from '../../layout/AppLayout.vue';
-    import { defineComponent, onBeforeMount } from 'vue';
-    import FileTable from '@/components/fileBrowser/FileTable.vue';
-    import ResultsTable from '@/components/fileBrowser/ResultsTable.vue';
-    import { updateContent, searchResults, sharedDir, selectedPaths, searchDirValue } from '@/store/fileBrowserStore';
-    import TopBar from '@/components/fileBrowser/TopBar.vue';
-    import SharedContent from '@/components/fileBrowser/SharedContent.vue';
+<script  lang="ts">
+import appLayout from '../../layout/AppLayout.vue';
+import { computed, defineComponent, onBeforeMount } from 'vue';
+import FileTable from '@/components/fileBrowser/FileTable.vue';
+import ResultsTable from '@/components/fileBrowser/ResultsTable.vue';
+import {
+    updateContent,
+    searchResults,
+    sharedDir,
+    selectedPaths,
+    searchDirValue,
+    currentDirectory,
+    currentShare,
+} from '@/store/fileBrowserStore';
+import TopBar from '@/components/fileBrowser/TopBar.vue';
+import SharedContent from '@/components/fileBrowser/SharedContent.vue';
+import router from '@/plugins/Router';
+import { useRoute, useRouter } from 'vue-router';
+import { isUndefined } from 'lodash';
 
-    export default defineComponent({
-        name: 'Apps',
-        components: {
-            TopBar,
-            appLayout,
-            FileTable,
-            ResultsTable,
-            SharedContent,
-        },
-        setup() {
-            onBeforeMount(async () => {
-                await updateContent();
-                sharedDir.value = false;
-                selectedPaths.value = [];
-                searchResults.value = [];
-                searchDirValue.value = '';
-            });
+export default defineComponent({
+    name: 'Apps',
+    components: {
+        TopBar,
+        appLayout,
+        FileTable,
+        ResultsTable,
+        SharedContent,
+    },
 
-            return {
-                searchResults,
-                sharedDir,
-            };
-        },
-    });
+    setup() {
+        const route = useRoute();
+        const router = useRouter();
+
+        onBeforeMount(async () => {
+            currentDirectory.value = route.params.path;
+            if (isUndefined(currentDirectory.value)) currentDirectory.value = '/';
+            await updateContent(currentDirectory.value); // bug to fix
+            sharedDir.value = false;
+            selectedPaths.value = [];
+            searchResults.value = [];
+            searchDirValue.value = '';
+        });
+
+        return {
+            searchResults,
+            sharedDir,
+        };
+    },
+});
 </script>
 
 <style scoped type="text/css"></style>
