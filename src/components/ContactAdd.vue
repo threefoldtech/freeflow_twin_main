9820bc00191ae4c8afc4c08baa2acc4d1c7a7dc6
bertmeeuws <bert.meeuws@outlook.com>
2021-10-01 16:29:48 +0200

    Refactoring code, and styling the contact add for visual consistency #123

--- src/components/ContactAdd.vue
+++ src/components/ContactAdd.vue
@@ -2,99 +2,147 @@
     <div class="place-items-start">
         <div class="grid grid-cols-2">
             <a
-                :class="{ active: isActive('user') }"
-                class="nav-link grid-cols-6 text-center py-2"
+                v-for="item in navigation"
+                :key="item.name"
+                class="nav-link grid-cols-6 text-center py-2 rounded-xl font-normal"
+                :class="[activeItem === item.name ? 'bg-primary text-white' : 'bg-gray-100 text-black']"
                 href="#"
-                @click.prevent="setActive('user')"
+                @click.prevent="setActive(item.name)"
             >
-                Add a user
-            </a>
-            <a
-                :class="{ active: isActive('group') }"
-                class="nav-link grid-cols-6 text-center py-2"
-                href="#"
-                @click.prevent="setActive('group')"
-            >
-                Create a group
+                {{ item.text }}
             </a>
         </div>
+        <div v-if="isActive('user')" class="flex flex-col">
+            <user-table
+                :data="possibleUsers"
+                placeholder="Search for user..."
+                @addContact="contactAdd"
+                focus
+            ></user-table>
+            <Disclosure v-slot="{ open }">
+                <DisclosureButton
+                    class="
+                        flex
+                        justify-between
+                        w-full
+                        mt-4
+                        ml-0
+                        py-2
+                        text-sm
+                        font-medium
+                        text-left text-gray-500
+                        bg-gray-50
+                        rounded-lg
+                        hover:bg-gray-100
+                        focus:outline-none
+                        focus-visible:ring focus-visible:ring-gray-500 focus-visible:ring-opacity-75
+                    "
+                >
+                    <span class="ml-6">Advanced</span>
 
-            <div v-if="isActive('user')" class="flex flex-col">
-                <user-table
-                    :data="possibleUsers"
-                    placeholder="Search for user..."
-                    @addContact="contactAdd"
-                    focus
-                ></user-table>
-                <Disclosure v-slot="{ open }">
-                    <DisclosureButton
-                        class="
-                            flex
-                            justify-between
-                            w-full
-                            mt-4
-                            ml-0
-                            py-2
-                            text-sm
-                            font-medium
-                            text-left text-gray-500
-                            bg-gray-50
-                            rounded-lg
-                            hover:bg-gray-100
-                            focus:outline-none
-                            focus-visible:ring focus-visible:ring-gray-500 focus-visible:ring-opacity-75
-                        "
-                    >
-                        <span class="ml-6">Advanced</span>
-
-                        <ChevronUpIcon :class="{ 'rotate-180': !open }" class="w-5 h-5 text-gray-500 transform mx-2" />
-                    </DisclosureButton>
-                    <DisclosurePanel class="px-4 pt-4 pb-2 text-sm text-gray-500">
+                    <ChevronUpIcon :class="{ 'rotate-180': !open }" class="w-5 h-5 text-gray-500 transform mx-2" />
+                </DisclosureButton>
+                <DisclosurePanel class="px-4 pt-4 pb-2 text-sm text-gray-500">
+                    <div>
                         <div>
+                            <label for="manualContactAddUsername" class="block text-sm font-medium text-gray-700"
+                                >Name</label
+                            >
                             <div>
-                                <label for="manualContactAddUsername" class="block text-sm font-medium text-gray-700">Name</label>
-                                <div>
-                                    <input type="text" name="manualContactAddUsername" id="manualContactAddUsername" v-model="manualContactAddUsername" class="shadow-sm focus:ring-primary focus:border-primary block w-full sm:text-sm border-gray-300 rounded-md mt-1" placeholder="Username" />
-                                    <span class="text-red-600" v-if="error != ''"> {{ usernameAddError }} </span>
-                                </div>
-                                <label for="manualContactAddLocation" class="block text-sm font-medium text-gray-700">Location</label>
-                                <div>
-                                    <input type="text" name="manualContactAddLocation" id="manualContactAddLocation" v-model="manualContactAddLocation" class="shadow-sm focus:ring-primary focus:border-primary block w-full sm:text-sm border-gray-300 rounded-md mt-1" placeholder="Location" />
-                                </div>
-                            </div>
-                            <div class="w-full flex justify-end">
-                                <button
+                                <input
+                                    type="text"
+                                    name="manualContactAddUsername"
+                                    id="manualContactAddUsername"
+                                    v-model="manualContactAddUsername"
                                     class="
-                                        w-auto
-                                        px-3
-                                        py-2
-                                        mt-2
-                                        border
-                                        cursor-pointer
-                                        border-transparent
-                                        text-sm
-                                        leading-4
-                                        font-medium
+                                        shadow-sm
+                                        focus:ring-primary focus:border-primary
+                                        block
+                                        w-full
+                                        sm:text-sm
+                                        border-gray-300
                                         rounded-md
-                                        text-white
-                                        bg-primary
-                                        focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-accent-500
+                                        mt-1
                                     "
-                                    value=""
-                                    @click="contactAdd"
-                                >Manually add</button>
+                                    placeholder="Username"
+                                />
+                                <span class="text-red-600" v-if="error != ''"> {{ usernameAddError }} </span>
+                            </div>
+                            <label for="manualContactAddLocation" class="block text-sm font-medium text-gray-700"
+                                >Location</label
+                            >
+                            <div>
+                                <input
+                                    type="text"
+                                    name="manualContactAddLocation"
+                                    id="manualContactAddLocation"
+                                    v-model="manualContactAddLocation"
+                                    class="
+                                        shadow-sm
+                                        focus:ring-primary focus:border-primary
+                                        block
+                                        w-full
+                                        sm:text-sm
+                                        border-gray-300
+                                        rounded-md
+                                        mt-1
+                                    "
+                                    placeholder="Location"
+                                />
                             </div>
                         </div>
-                    </DisclosurePanel>
-                </Disclosure>
-            </div>
+                        <div class="w-full flex justify-end">
+                            <button
+                                class="
+                                    w-auto
+                                    px-3
+                                    py-2
+                                    mt-2
+                                    border
+                                    cursor-pointer
+                                    border-transparent
+                                    text-sm
+                                    leading-4
+                                    font-medium
+                                    rounded-md
+                                    text-white
+                                    bg-primary
+                                    focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-accent-500
+                                "
+                                value=""
+                                @click="contactAdd"
+                            >
+                                Manually add
+                            </button>
+                        </div>
+                    </div>
+                </DisclosurePanel>
+            </Disclosure>
+        </div>
         <form v-if="isActive('group')" class="w-full overflow-x-hidden" @submit.prevent="groupAdd">
             <div class="flex place-items-center mx-1">
                 <div class="w-full">
                     <div>
                         <label for="groupname" class="block text-sm font-medium text-gray-700">Group name</label>
                         <div>
-                            <input type="text" name="groupname" id="groupname" v-model="groupnameAdd" class="shadow-sm focus:ring-primary focus:border-primary block w-full sm:text-sm border-gray-300 rounded-md mt-1" placeholder="Group name" v-focus/>
+                            <input
+                                type="text"
+                                name="groupname"
+                                id="groupname"
+                                v-model="groupnameAdd"
+                                class="
+                                    shadow-sm
+                                    focus:ring-primary focus:border-primary
+                                    block
+                                    w-full
+                                    sm:text-sm
+                                    border-gray-300
+                                    rounded-md
+                                    mt-1
+                                "
+                                placeholder="Group name"
+                                v-focus
+                            />
                         </div>
                     </div>
                     <br />
@@ -141,7 +189,6 @@
     import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue';
     import { ChevronUpIcon } from '@heroicons/vue/solid';
 
-
     export default defineComponent({
         name: 'ContactAdd',
         components: {
@@ -157,34 +204,41 @@
         setup(props, { emit }) {
             const { contacts } = useContactsState();
             //const contacts = [{"id":"jens", "location":"145.546.487"},{"id":"Simon", "location":"145.586.487"},{"id":"jonas", "location":"145.546.48765654654"},{"id":"Ine", "location":"145.546sdfsdf.487"}];
-            let addGroup = ref(false);
-            let userAddLocation = ref('');
-            let usernameAddError = ref('');
-            let groupnameAdd = ref('');
-            let groupnameAddError = ref('');
-            let usernameInGroupAdd = ref('');
-            let usersInGroup = ref<Contact[]>([]);
-            let possibleUsers = ref<Contact[]>([]);
-            let contactAddError = ref('');
+            const addGroup = ref(false);
+            const userAddLocation = ref('');
+            const usernameAddError = ref('');
+            const groupnameAdd = ref('');
+            const groupnameAddError = ref('');
+            const usernameInGroupAdd = ref('');
+            const usersInGroup = ref<Contact[]>([]);
+            const possibleUsers = ref<Contact[]>([]);
+            const contactAddError = ref('');
 
-            let manualContactAddUsername = ref<string>('');
-            let manualContactAddLocation = ref('');
+            const manualContactAddUsername = ref<string>('');
+            const manualContactAddLocation = ref('');
 
-            const contactAdd = (contact:Contact) => {
-                const contactToAdd:Contact = {
+            const navigation = ref([
+                { name: 'user', text: 'Add a user' },
+                { name: 'group', text: 'Create a group' },
+            ]);
+
+            const contactAdd = (contact: Contact) => {
+                const contactToAdd: Contact = {
                     id: contact?.id ? contact.id : manualContactAddUsername.value,
-                    location:  contact?.location ? contact.location : manualContactAddLocation.value
-                }
-                console.log("contact to add ", contactToAdd)
+                    location: contact?.location ? contact.location : manualContactAddLocation.value,
+                };
+                console.log('contact to add ', contactToAdd);
                 try {
                     const { chats } = usechatsState();
 
-                    if (chats.value.filter(chat => !chat.isGroup).find(chat => <string>chat.chatId == contactToAdd.id)) {
+                    if (
+                        chats.value.filter(chat => !chat.isGroup).find(chat => <string>chat.chatId == contactToAdd.id)
+                    ) {
                         usernameAddError.value = 'Already added this user';
                         return;
                     }
                     const { addContact } = useContactsActions();
-                    addContact( contactToAdd.id  ,contactToAdd.location);
+                    addContact(contactToAdd.id, contactToAdd.location);
                     manualContactAddUsername.value = undefined;
                     contactAddError.value = '';
                     emit('closeDialog');
@@ -200,11 +254,12 @@
                 }
             };
 
-            const handleClicked = (item) => {
-                    userAddLocation.value = item.location;
+            const handleClicked = item => {
+                userAddLocation.value = item.location;
             };
 
-            let activeItem = ref('user');
+            const activeItem = ref('user');
+
             const isActive = menuItem => {
                 return activeItem.value === menuItem;
             };
@@ -247,8 +302,8 @@
             axios.get(`${config.appBackend}api/users/digitaltwin`, {}).then(r => {
                 const { user } = useAuthState();
                 const posContacts = <Contact[]>r.data;
-                const alreadyExistingChatIds = [...contacts.map(c => c.id),user.id]
-                possibleUsers.value = posContacts.filter(pu => !alreadyExistingChatIds.find(aEx =>aEx === pu.id));
+                const alreadyExistingChatIds = [...contacts.map(c => c.id), user.id];
+                possibleUsers.value = posContacts.filter(pu => !alreadyExistingChatIds.find(aEx => aEx === pu.id));
             });
 
             return {
@@ -268,13 +323,11 @@
                 handleClicked,
                 manualContactAddUsername,
                 manualContactAddLocation,
+                activeItem,
+                navigation,
             };
         },
     });
 </script>
 
-<style scoped>
-    a.active {
-        background: #e5e7eb;
-    }
-</style>
+<style scoped></style>
