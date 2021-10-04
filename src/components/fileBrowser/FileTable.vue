e5df1881b12b7977af5406a4f4c6d4be813c9d25
bertmeeuws <bert.meeuws@outlook.com>
2021-10-04 16:26:54 +0200

    Grid in local filebrowser WOIP

--- src/components/fileBrowser/FileTable.vue
+++ src/components/fileBrowser/FileTable.vue
@@ -1,8 +1,20 @@
 <template>
     <div class="flex flex-col mx-2">
         <div class="overflow-x-auto">
-            <div class="py-2 align-middle inline-block min-w-full">
-                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <div class="py-2 px-4 align-middle inline-block min-w-full">
+                <div class="flex justify-end mb-2 hidden">
+                    <ViewListIcon
+                        class="h-6 w-6 text-gray-400 hover:text-primary transition duration-100 cursor-pointer"
+                        :class="{ 'text-primary': fileBrowserTypeView === 'LIST' }"
+                        @click="changeView('LIST')"
+                    />
+                    <ViewGridIcon
+                        class="h-6 w-6 text-gray-400 hover:text-primary transition duration-100 cursor-pointer"
+                        :class="{ 'text-primary': fileBrowserTypeView === 'GRID' }"
+                        @click="changeView('GRID')"
+                    />
+                </div>
+                <div class="overflow-hidden border-b border-gray-200 sm:rounded-lg">
                     <FileDropArea class="h-full" @send-file="uploadFiles">
                         <div ref="hiddenItems" class="absolute hiddenItems">
                             <div ref="ghostImage" class="bg-white p-2">
@@ -11,7 +23,7 @@
                         </div>
                         <table
                             :key="currentDirectory"
-                            class="min-w-full divide-y divide-gray-200"
+                            class="min-w-full divide-y divide-gray-200 shadow"
                             @dragenter="onDragEnterParent"
                             @dragleave="onDragLeaveParent"
                         >
@@ -182,6 +194,89 @@
                                 </tr>
                             </tbody>
                         </table>
+                        <!--
+                        <ul
+                            role="list"
+                            v-else
+                            class="
+                                grid grid-cols-2
+                                gap-x-4 gap-y-8
+                                sm:grid-cols-3 sm:gap-x-6
+                                lg:grid-cols-4
+                                xl:gap-x-8
+                                mt-4
+                            "
+                        >
+                            <p
+                                class="
+                                    px-6
+                                    py-4
+                                    whitespace-nowrap
+                                    col-span-4
+                                    text-base
+                                    font-medium
+                                    text-center text-gray-800
+                                    flex
+                                    justify-center
+                                    flex-col
+                                "
+                                v-if="sortContent().length === 0"
+                            >
+                                No items in this folder
+                                <span @click="$router.go(-1)" class="mt-4 underline cursor-pointer">Go back</span>
+                            </p>
+                            <li
+                                v-for="item in sortContent()"
+                                :key="item.fullName"
+                                draggable="true"
+                                @dragover="event => onDragOver(event, item)"
+                                @dragstart="event => onDragStart(event, item)"
+                                @drop="() => onDrop(item)"
+                                class="relative"
+                                @click="handleItemClick(item)"
+                            >
+                                <div
+                                    class="
+                                        group
+                                        w-full
+                                        aspect-w-10 aspect-h-7
+                                        rounded-lg
+                                        bg-gray-200
+                                        hover:bg-gray-300
+                                        transition
+                                        duration:200
+                                        focus-within:ring-2
+                                        focus-within:ring-offset-2
+                                        focus-within:ring-offset-gray-100
+                                        focus-within:ring-indigo-500
+                                        overflow-hidden
+                                        flex
+                                        justify-center
+                                        items-center
+                                    "
+                                    @click="handleSelect(item)"
+                                >
+                                    <div class="flex justify-center items-center cursor-pointer">
+                                        <i
+                                            :key="item.name"
+                                            class="fa-2x"
+                                            :class="getIcon(item) + ' ' + getIconColor(item)"
+                                        ></i>
+                                        <button type="button" class="absolute inset-0 focus:outline-none">
+                                            <span class="sr-only">View details for {{ item.name }}</span>
+                                        </button>
+                                    </div>
+                                </div>
+                                <p class="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">
+                                    {{ item.name
+                                    }}{{ getFileExtension(item) === '-' ? '' : `.${getFileExtension(item)}` }}
+                                </p>
+                                <p class="block text-sm font-medium text-gray-500 pointer-events-none">
+                                    {{ getFileLastModified(item) }}
+                                </p>
+                            </li>
+                        </ul>
+                        -->
                     </FileDropArea>
                 </div>
             </div>
@@ -190,188 +285,200 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onBeforeMount, ref } from 'vue';
-import {
-    currentDirectory,
-    currentDirectoryContent,
-    itemAction,
-    PathInfoModel,
-    selectItem,
-    deselectAll,
-    selectAll,
-    selectedPaths,
-    deselectItem,
-    sortContent,
-    sortAction,
-    currentSort,
-    currentSortDir,
-    getFileLastModified,
-    getFileExtension,
-    getFileSize,
-    getIconColor,
-    getIcon,
-    uploadFiles,
-    equals,
-    moveFiles,
-    isDraggingFiles,
-    sharedDir,
-    sharedContent,
-    getSharedContent,
-    searchResults,
-    searchDirValue,
-    currentShare,
-    goToShared,
-} from '@/store/fileBrowserStore';
-import { useRouter } from 'vue-router';
-import FileDropArea from '@/components/FileDropArea.vue';
-import { useSocketActions } from '@/store/socketStore';
-import { useAuthState } from '@/store/authStore';
+    import { defineComponent, onBeforeMount, ref } from 'vue';
+    import { ViewGridIcon, ViewListIcon } from '@heroicons/vue/solid';
+    import {
+        currentDirectory,
+        currentDirectoryContent,
+        itemAction,
+        PathInfoModel,
+        selectItem,
+        deselectAll,
+        selectAll,
+        selectedPaths,
+        deselectItem,
+        sortContent,
+        sortAction,
+        currentSort,
+        currentSortDir,
+        getFileLastModified,
+        getFileExtension,
+        getFileSize,
+        getIconColor,
+        getIcon,
+        uploadFiles,
+        equals,
+        moveFiles,
+        isDraggingFiles,
+        sharedDir,
+        sharedContent,
+        getSharedContent,
+        searchResults,
+        searchDirValue,
+        currentShare,
+        goToShared,
+        fileBrowserTypeView,
+    } from '@/store/fileBrowserStore';
+    import { useRouter } from 'vue-router';
+    import FileDropArea from '@/components/FileDropArea.vue';
+    import { useSocketActions } from '@/store/socketStore';
+    import { useAuthState } from '@/store/authStore';
 
-export default defineComponent({
-    name: 'FileTable',
-    computed: {
-        orderClass() {
-            return this.currentSortDir === 'asc' ? 'arrow asc' : 'arrow desc';
+    export default defineComponent({
+        name: 'FileTable',
+        computed: {
+            orderClass() {
+                return this.currentSortDir === 'asc' ? 'arrow asc' : 'arrow desc';
+            },
         },
-    },
-    components: { FileDropArea },
-    setup() {
-        const hiddenItems = ref<HTMLDivElement>();
-        const ghostImage = ref<HTMLDivElement>();
-        const dragOverItem = ref<PathInfoModel>();
-        let tempCounter = 0;
-        const router = useRouter();
-        const { user } = useAuthState();
-        onBeforeMount(() => {
-            const { initializeSocket } = useSocketActions();
-            initializeSocket(user.id.toString());
-        });
+        components: { FileDropArea, ViewGridIcon, ViewListIcon },
+        setup() {
+            const hiddenItems = ref<HTMLDivElement>();
+            const ghostImage = ref<HTMLDivElement>();
+            const dragOverItem = ref<PathInfoModel>();
+            let tempCounter = 0;
+            const router = useRouter();
+            const { user } = useAuthState();
+            onBeforeMount(() => {
+                const { initializeSocket } = useSocketActions();
+                initializeSocket(user.id.toString());
+            });
 
-        const handleSelect = (item: PathInfoModel) => {
-            if (!selectedPaths.value.includes(item)) selectItem(item);
-            else deselectItem(item);
-        };
+            const handleSelect = (item: PathInfoModel) => {
+                if (!selectedPaths.value.includes(item)) selectItem(item);
+                else deselectItem(item);
+            };
 
-        const isSelected = (item: PathInfoModel) => {
-            if (!selectedPaths.value.includes(item)) return false;
-            else return true;
-        };
+            const isSelected = (item: PathInfoModel) => {
+                if (!selectedPaths.value.includes(item)) return false;
+                else return true;
+            };
 
-        const handleAllSelect = (val: any) => {
-            if (val.target.checked) selectAll();
-            else deselectAll();
-        };
+            const handleAllSelect = (val: any) => {
+                if (val.target.checked) selectAll();
+                else deselectAll();
+            };
 
-        const handleItemClick = (item: PathInfoModel) => {
-            itemAction(item, router);
-        };
+            const handleItemClick = (item: PathInfoModel) => {
+                itemAction(item, router);
+            };
 
-        const onDragStart = (event, item) => {
-            isDraggingFiles.value = true;
-            if (!selectedPaths.value.includes(item)) selectItem(item);
-            event.dataTransfer.setDragImage(ghostImage.value, 0, 0);
-        };
+            const onDragStart = (event, item) => {
+                isDraggingFiles.value = true;
+                if (!selectedPaths.value.includes(item)) selectItem(item);
+                event.dataTransfer.setDragImage(ghostImage.value, 0, 0);
+            };
 
-        const onDragOver = (event: Event, item: PathInfoModel) => {
-            dragOverItem.value = item;
-        };
+            const onDragOver = (event: Event, item: PathInfoModel) => {
+                dragOverItem.value = item;
+            };
 
-        const canBeDropped = (item: PathInfoModel) => {
-            return item.isDirectory && selectedPaths.value.findIndex(x => equals(x, item)) === -1;
-        };
+            const canBeDropped = (item: PathInfoModel) => {
+                return item.isDirectory && selectedPaths.value.findIndex(x => equals(x, item)) === -1;
+            };
 
-        const onDragLeaveParent = () => {
-            tempCounter--;
-            if (tempCounter === 0) dragOverItem.value = undefined;
-        };
+            const onDragLeaveParent = () => {
+                tempCounter--;
+                if (tempCounter === 0) dragOverItem.value = undefined;
+            };
 
-        const onDragEnterParent = () => {
-            tempCounter++;
-        };
+            const onDragEnterParent = () => {
+                tempCounter++;
+            };
 
-        const highlight = (item: PathInfoModel) => {
-            return equals(item, dragOverItem.value) && canBeDropped(item);
-        };
+            const highlight = (item: PathInfoModel) => {
+                return equals(item, dragOverItem.value) && canBeDropped(item);
+            };
 
-        const onDrop = (item: PathInfoModel) => {
-            tempCounter = 0;
-            if (!canBeDropped(item)) return;
-            dragOverItem.value = undefined;
-            moveFiles(
-                item.path,
-                selectedPaths.value.map(x => x.path)
-            );
-            selectedPaths.value = [];
-        };
+            const onDrop = (item: PathInfoModel) => {
+                tempCounter = 0;
+                if (!canBeDropped(item)) return;
+                dragOverItem.value = undefined;
+                moveFiles(
+                    item.path,
+                    selectedPaths.value.map(x => x.path)
+                );
+                selectedPaths.value = [];
+            };
 
-        const onDragEnd = () => {
-            isDraggingFiles.value = false;
-        };
+            const onDragEnd = () => {
+                isDraggingFiles.value = false;
+            };
 
-        return {
-            handleSelect,
-            isSelected,
-            handleAllSelect,
-            handleItemClick,
-            currentDirectoryContent,
-            currentDirectory,
-            selectedPaths,
-            sortContent,
-            sortAction,
-            currentSort,
-            currentSortDir,
-            uploadFiles,
-            getFileLastModified,
-            getFileExtension,
-            getFileSize,
-            getIconColor,
-            getIcon,
-            onDragStart,
-            hiddenItems,
-            ghostImage,
-            onDragOver,
-            equals,
-            dragOverItem,
-            highlight,
-            onDrop,
-            onDragEnterParent,
-            onDragLeaveParent,
-            onDragEnd,
-            sharedDir,
-            sharedContent,
-            goToShared,
-        };
-    },
-});
+            const changeView = (type: string) => {
+                fileBrowserTypeView.value = type;
+            };
+
+            const goBack = () => {};
+
+            return {
+                handleSelect,
+                isSelected,
+                handleAllSelect,
+                handleItemClick,
+                currentDirectoryContent,
+                currentDirectory,
+                selectedPaths,
+                sortContent,
+                sortAction,
+                currentSort,
+                currentSortDir,
+                uploadFiles,
+                getFileLastModified,
+                getFileExtension,
+                getFileSize,
+                getIconColor,
+                getIcon,
+                onDragStart,
+                hiddenItems,
+                ghostImage,
+                onDragOver,
+                equals,
+                dragOverItem,
+                highlight,
+                onDrop,
+                onDragEnterParent,
+                onDragLeaveParent,
+                onDragEnd,
+                sharedDir,
+                sharedContent,
+                goToShared,
+                fileBrowserTypeView,
+                changeView,
+                goBack,
+                router,
+            };
+        },
+    });
 </script>
 
 <style scoped>
-th.active .arrow {
-    opacity: 1;
-}
+    th.active .arrow {
+        opacity: 1;
+    }
 
-.arrow {
-    display: inline-block;
-    vertical-align: middle;
-    width: 0;
-    height: 0;
-    margin-left: 5px;
-    opacity: 0;
-}
+    .arrow {
+        display: inline-block;
+        vertical-align: middle;
+        width: 0;
+        height: 0;
+        margin-left: 5px;
+        opacity: 0;
+    }
 
-.arrow.asc {
-    border-left: 4px solid transparent;
-    border-right: 4px solid transparent;
-    border-bottom: 4px solid #1f0f5b;
-}
+    .arrow.asc {
+        border-left: 4px solid transparent;
+        border-right: 4px solid transparent;
+        border-bottom: 4px solid #1f0f5b;
+    }
 
-.arrow.desc {
-    border-left: 4px solid transparent;
-    border-right: 4px solid transparent;
-    border-top: 4px solid #1f0f5b;
-}
+    .arrow.desc {
+        border-left: 4px solid transparent;
+        border-right: 4px solid transparent;
+        border-top: 4px solid #1f0f5b;
+    }
 
-.hiddenItems {
-    z-index: -20;
-}
+    .hiddenItems {
+        z-index: -20;
+    }
 </style>
--- src/components/fileBrowser/ResultsTable.vue
+++ src/components/fileBrowser/ResultsTable.vue
@@ -2,12 +2,20 @@
     <h1 class="p-2">Search results for {{ searchDirValue }}</h1>
     <div class="overflow-x-auto">
         <div class="py-2 align-middle inline-block min-w-full">
+            <div class="flex justify-end mb-2">
+                <ViewListIcon
+                    class="h-6 w-6 text-gray-400 hover:text-primary transition duration-100 cursor-pointer"
+                    :class="{ 'text-primary': fileBrowserTypeView === 'LIST' }"
+                    @click="changeView('LIST')"
+                />
+                <ViewGridIcon
+                    class="h-6 w-6 text-gray-400 hover:text-primary transition duration-100 cursor-pointer"
+                    :class="{ 'text-primary': fileBrowserTypeView === 'GRID' }"
+                    @click="changeView('GRID')"
+                />
+            </div>
             <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                <table
-                    v-if="fileBrowserTypeView === 'LIST'"
-                    class="min-w-full divide-y divide-gray-200"
-                    :key="currentDirectory"
-                >
+                <table class="min-w-full divide-y divide-gray-200" :key="currentDirectory">
                     <thead class="bg-gray-50">
                         <tr>
                             <th
