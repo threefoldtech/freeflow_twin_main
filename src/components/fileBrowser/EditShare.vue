52cc76e16dc024a54c696a0f16139e7f7bebe243
ldruwel <laurens.druwel@jimber.org>
2021-10-05 21:26:01 +0200

    removeshare

--- src/components/fileBrowser/EditShare.vue
+++ src/components/fileBrowser/EditShare.vue
@@ -15,109 +15,117 @@
     <Table v-if="!isLoading" :headers="headers" :data="searchResults">
         <template #data-types="data">
             <div class="my-1 p-2 rounded-md border border-gray-200 w-20">
-                <span v-if="canWrite(data.data)">Can write</span>
-                <span v-else>Can read</span>
+                <span v-if="canWrite(data.data)">Write</span>
+                <span v-else>Read</span>
             </div>
             <!-- <div class="cursor-pointer rounded-xl bg-gray-50 border border-gray-200 w-28 justify-between flex content-center items-center ">
                 <span @click="item.canWrite = false" class="p-2 rounded-xl" :class="{ 'bg-primary text-white': data.data.length <=1 }"> Read</span>
                 <span @click="item.canWrite = true" class="p-2 rounded-xl" :class="{ 'bg-primary text-white': data.data.length > 1}"> Write</span>
             </div> -->
         </template>
-        <template #data-delete>
-            <span class="my-1 p-2 rounded-md bg-red-500 text-white"> Remove </span>
+        <template #data-delete="data">
+            <span class="my-1 p-2 rounded-md bg-red-500 text-white" @click="remove(data.row)"> Remove </span>
         </template>
     </Table>
     <div v-else class="flex justify-center itemns-center mt-2">This file isn't shared with anyone yet.</div>
 </template>
 <script lang="ts">
-    import { Chat, SharedFileInterface } from '@/types';
-    import { selectedPaths, addShare } from '@/store/fileBrowserStore';
-    import { defineComponent, ref, computed, onMounted, onBeforeMount } from 'vue';
-    import Toggle from '@/components/Toggle.vue';
-    import { CheckIcon, SelectorIcon } from '@heroicons/vue/solid';
-    import { sendMessageObject, usechatsActions, usechatsState } from '@/store/chatStore';
-    import AvatarImg from '@/components/AvatarImg.vue';
-    import { SystemMessageTypes, MessageTypes } from '@/types';
-    const { sendMessage } = usechatsActions();
-    import { createNotification } from '@/store/notificiationStore';
-    import { Table, IHeader, TEntry } from '@jimber/shared-components';
-    import { isObject } from 'lodash';
-    import { getShareByPath } from '@/services/fileBrowserService';
-    import { SearchIcon } from '@heroicons/vue/solid';
+import { Chat, SharedFileInterface } from '@/types';
+import { selectedPaths, addShare } from '@/store/fileBrowserStore';
+import { defineComponent, ref, computed, onMounted, onBeforeMount } from 'vue';
+import Toggle from '@/components/Toggle.vue';
+import { CheckIcon, SelectorIcon } from '@heroicons/vue/solid';
+import { sendMessageObject, usechatsActions, usechatsState } from '@/store/chatStore';
+import AvatarImg from '@/components/AvatarImg.vue';
+import { SystemMessageTypes, MessageTypes } from '@/types';
+const { sendMessage } = usechatsActions();
+import { createNotification } from '@/store/notificiationStore';
+import { Table, IHeader, TEntry } from '@jimber/shared-components';
+import { isObject } from 'lodash';
+import { getShareByPath, removeShare } from '@/services/fileBrowserService';
+import { SearchIcon } from '@heroicons/vue/solid';
+import { useRoute } from 'vue-router';
 
-    const headers: IHeader<TEntry>[] = [
-        {
-            key: 'chatId',
-            displayName: 'Chat',
-            enableSorting: true,
-        },
-        {
-            key: 'types',
-            displayName: 'Permission',
-        },
-        {
-            key: 'delete',
-            displayName: 'Delete',
-        },
-    ];
+const headers: IHeader<TEntry>[] = [
+    {
+        key: 'chatId',
+        displayName: 'Chat',
+        enableSorting: true,
+    },
+    {
+        key: 'types',
+        displayName: 'Permission',
+    },
+    {
+        key: 'delete',
+        displayName: 'Delete',
+    },
+];
 
-    export default defineComponent({
-        components: { SearchIcon, Toggle, AvatarImg, Table },
-        props: {
-            selectedFile: {
-                type: Object,
-                required: true,
-            },
+export default defineComponent({
+    components: { SearchIcon, Toggle, AvatarImg, Table },
+    props: {
+        selectedFile: {
+            type: Object,
+            required: true,
         },
-        emits: ['update:modelValue', 'clicked'],
+    },
+    emits: ['update:modelValue', 'clicked'],
 
-        setup(props, { emit }) {
-            const searchTerm = ref('');
-            const isLoading = ref(true);
-            const currentShare = ref<SharedFileInterface>();
+    setup(props, { emit }) {
+        const searchTerm = ref('');
+        const isLoading = ref(true);
+        const currentShare = ref<SharedFileInterface>();
 
-            onBeforeMount(async () => {
-                currentShare.value = await getShareByPath(props.selectedFile.path);
-                isLoading.value = false;
+        onBeforeMount(async () => {
+            currentShare.value = await getShareByPath(props.selectedFile.path);
+            isLoading.value = false;
+        });
+
+        const reset = () => {
+            emit('update:modelValue', '');
+            searchTerm.value = '';
+        };
+
+        const handleInput = evt => {
+            emit('update:modelValue', evt.target.value);
+        };
+
+        const searchResults = computed(() => {
+            return currentShare.value.permissions.filter(item => {
+                return item.chatId.toLowerCase().includes(searchTerm.value.toLowerCase());
             });
+        });
 
-            const reset = () => {
-                emit('update:modelValue', '');
-                searchTerm.value = '';
+        const canWrite = computed(() => {
+            return param => {
+                return !!param.find(perm => perm == 'w');
             };
+        });
 
-            const handleInput = evt => {
-                emit('update:modelValue', evt.target.value);
-            };
+        const remove = (data: any) => {
+            console.log(data.chatId);
+            console.log(selectedPaths.value[0]);
+            removeShare(data.chatId, selectedPaths.value[0].path);
+        };
 
-            const searchResults = computed(() => {
-                return currentShare.value.permissions.filter(item => {
-                    return item.chatId.toLowerCase().includes(searchTerm.value.toLowerCase());
-                });
-            });
-
-            const canWrite = computed(() => {
-                return param => {
-                    return !!param.find(perm => perm == 'w');
-                };
-            });
-
-            return {
-                reset,
-                handleInput,
-                searchTerm,
-                searchResults,
-                selectedPaths,
-                headers,
-                isLoading,
-                canWrite,
-            };
-        },
-    });
+        return {
+            reset,
+            handleInput,
+            searchTerm,
+            searchResults,
+            selectedPaths,
+            headers,
+            isLoading,
+            canWrite,
+            remove,
+        };
+    },
+});
 </script>
 
 <style scoped>
-    .mh-48 {
-        max-height: 10rem;
-    }
+.mh-48 {
+    max-height: 10rem;
+}
 </style>
