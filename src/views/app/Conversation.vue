f50a74a4b5c8c4fc361f707598020db1a762cb3d
Jonas Wijne <jonas.wijne@jimber.org>
2021-10-20 14:04:02 +0200

    add flex-grow

--- src/views/app/Conversation.vue
+++ src/views/app/Conversation.vue
@@ -206,7 +206,7 @@
                             <div class="h-full flex items-center justify-center"><i class="fa fa-times"></i></div>
                         </button>
                     </div>
-                    <div class="max-w-full w-full bg-white border flex flex-col">
+                    <div class="max-w-full w-full bg-white border flex flex-col flex-grow">
                         <div class="bg-white pb-4 w-full mb-4 p-6 flex min-h-64 justify-start relative">
                             <XIcon
                                 class="
