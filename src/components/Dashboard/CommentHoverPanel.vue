a59f08679297b5c2dae50b963c4274c7b9a4b372
bertmeeuws <bert.meeuws@outlook.com>
2021-12-23 12:29:06 +0100

    Small fixes

--- src/components/Dashboard/CommentHoverPanel.vue
+++ src/components/Dashboard/CommentHoverPanel.vue
@@ -3,7 +3,6 @@
       class="absolute -top-40 -left-10 mt-4 bg-white px-4 py-4 w-72 shadow rounded cursor-default z-50">
     <div class="flex space-x-3">
       <div class="flex flex-shrink-0">
-        {{ comment.avatar }}
         <img :src="avatar" alt="" class="h-16 w-16 object-fill rounded-full">
       </div>
       <div class="flex flex-col space-y-2">
--- src/components/Dashboard/PostComment.vue
+++ src/components/Dashboard/PostComment.vue
@@ -29,7 +29,7 @@
         </div>
         <div class="flex justify-start items-center text-xs w-full">
           <div class="font-semibold text-gray-700 px-2 flex items-center justify-center space-x-1">
-            <a @click="likePost" class="hover:underline" href="#">
+            <a  class="hover:underline" href="#">
               <small>Like</small>
             </a>
             <small v-if="comment.type === MESSAGE_TYPE.COMMENT" class="self-center">.</small>
@@ -109,9 +109,6 @@ const commentsSorted = computed(() => {
   });
 })
 
-const likePost = async () => {
-  await likeComment()
-}
 
 const avatarImg = computed(() => {
   return calcExternalResourceLink(`http://[${props.comment.owner.location}]/api/user/avatar/default`)
