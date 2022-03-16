cef20a58ae440544a2e24786306647b678641a99
EdwardKerckhof <edward@zonedev.be>
2022-03-16 17:50:28 +0100

    Auth endpoint

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -18,7 +18,7 @@ export const setNewAvatar = async selectedFile => {
 };
 
 export const isUserAuthenticated = async () => {
-    const url = `${config.baseUrl}api/auth/authenticated`;
+    const url = `${config.baseUrl}nest/auth/authenticated`;
     try {
         const result = await axios.get(url);
         return result.data;
