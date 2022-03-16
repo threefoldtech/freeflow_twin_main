ab66cf6065dff7ae36b21d23ba601e9502db1016
EdwardKerckhof <edward@zonedev.be>
2022-03-16 14:58:20 +0100

    Change blocked api endpoint

--- src/store/blockStore.ts
+++ src/store/blockStore.ts
@@ -6,20 +6,20 @@ export const blocklist = ref([]);
 
 export const initBlocklist = async () => {
     try {
-        const axiosResponse = await axios.get(`${config.baseUrl}api/blocked/`);
+        const axiosResponse = await axios.get(`${config.baseUrl}nest/blocked`);
         blocklist.value = axiosResponse.data;
     } catch (e) {
         console.log('could not get blocklist');
     }
 };
 
-export const addUserToBlockList = userid => {
-    blocklist.value.push(userid);
+export const addUserToBlockList = (userId: string) => {
+    blocklist.value.push(userId);
 };
 
 export const isBlocked = (userId: string) => blocklist.value.includes(userId);
 
-export const deleteBlockedEntry = async user => {
+export const deleteBlockedEntry = async (user: string) => {
     await axios.delete(`${config.baseUrl}api/blocked/${user}/`);
     blocklist.value = blocklist.value.filter(x => x !== user);
 };
