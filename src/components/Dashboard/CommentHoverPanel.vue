15b67362b5a3da6c1b012163bcbde9be67ffea7d
EdwardKerckhof <edward@zonedev.be>
2022-04-07 13:54:41 +0200

    Fix hardcoded online status in comment hover panel

--- src/components/Dashboard/CommentHoverPanel.vue
+++ src/components/Dashboard/CommentHoverPanel.vue
@@ -1,48 +1,57 @@
 <template>
-    <div
-        class='absolute mt-4 bg-white px-4 py-4 w-72 shadow rounded cursor-default z-50'>
-        <div class='flex space-x-3'>
-            <div class='flex flex-shrink-0'>
-                <img :src='avatar' alt='' class='h-16 w-16 object-fill rounded-full'>
+    <div class="absolute mt-4 bg-white px-4 py-4 w-72 shadow rounded cursor-default z-50">
+        <div class="flex space-x-3">
+            <div class="flex flex-shrink-0">
+                <img :src="avatar" alt="" class="h-16 w-16 object-fill rounded-full" />
             </div>
-            <div class='flex flex-col space-y-2'>
-                <div class='font-semibold text-base'>
-                    <a class='hover:underline' href='#'>
+            <div class="flex flex-col space-y-2">
+                <div class="font-semibold text-base">
+                    <a class="hover:underline" href="#">
                         {{ comment.owner.id }}
                     </a>
                 </div>
 
-                <div class='flex justify-start items-center space-x-2'>
-                    <div class='w-auto text-sm leading-none'>
-                        <small>
-                            Friends since <span class='font-medium'>21 November</span>
-                        </small>
+                <div class="flex justify-start items-center space-x-2">
+                    <div class="w-auto text-sm leading-none">
+                        <small> Friends since <span class="font-medium">21 November</span> </small>
                     </div>
                 </div>
-                <div class='flex justify-start items-center space-x-2'>
-                    <div class='w-auto text-sm leading-none'>
+                <div class="flex justify-start items-center space-x-2">
+                    <div class="w-auto text-sm leading-none">
                         <small>
-                            <span class='font-semibold text-green-700 text-sm'>Online</span>
+                            <span
+                                class="font-semibold text-sm"
+                                :class="{
+                                    'text-green-600': online,
+                                    'text-red-600': !online,
+                                }"
+                                >{{ online ? 'Online' : 'Offline' }}</span
+                            >
                         </small>
                     </div>
                 </div>
             </div>
         </div>
-        <div class='flex space-x-3 mt-4'>
-            <div class='w-auto' v-if='isPersonFriend !== null'>
-                <a class='text-xs text-accent-600 hover:bg-opacity-60 font-semibold flex items-center justify-center px-3 py-2 bg-accent-300 bg-opacity-50 rounded-lg'
-                   href='#'
-                   @click='goToChat'>
-                    <div class='mr-1 h-6 flex items-center flex-shrink-0'>
-                        <img class='' src='/whisperbold.svg' alt='whisper' />
+        <div class="flex space-x-3 mt-4">
+            <div class="w-auto" v-if="isPersonFriend !== null">
+                <a
+                    class="text-xs text-accent-600 hover:bg-opacity-60 font-semibold flex items-center justify-center px-3 py-2 bg-accent-300 bg-opacity-50 rounded-lg"
+                    href="#"
+                    @click="goToChat"
+                >
+                    <div class="mr-1 h-6 flex items-center flex-shrink-0">
+                        <img class="" src="/whisperbold.svg" alt="whisper" />
                     </div>
                     {{ isPersonFriend ? 'Whisper' : 'Add chat' }}
                 </a>
             </div>
-            <div class='w-auto'>
-                <a class='text-xs text-gray-800 hover:bg-gray-300 font-semibold w-10 h-10 flex items-center justify-center bg-gray-200 rounded-lg'
-                   href='#' @click='goToKutana'>
-                    <img class='w-6 h-6' src='/kutanabold.svg' alt='kutana' />
+            <div class="w-auto">
+                <a
+                    class="text-xs text-gray-800 hover:bg-gray-300 font-semibold w-10 h-10 flex items-center justify-center bg-gray-200 rounded-lg"
+                    href="#"
+                    @click="goToKutana"
+                >
+                    <img class="w-6 h-6" src="/kutanabold.svg" alt="kutana" />
                 </a>
             </div>
             <!--      <div class="w-auto">-->
@@ -55,7 +64,7 @@
     </div>
 </template>
 
-<script lang='ts' setup>
+<script lang="ts" setup>
     // import {DotsHorizontalIcon} from "@heroicons/vue/solid";
     import { useRouter } from 'vue-router';
     import { useContactsActions, useContactsState } from '@/store/contactStore';
@@ -63,21 +72,25 @@
     import { computed, nextTick, onBeforeMount, ref } from 'vue';
     import { myYggdrasilAddress } from '@/store/authStore';
     import { SOCIAL_POST } from '@/store/socialStore';
+    import { fetchStatus } from '@/store/statusStore';
 
     const { contacts } = useContactsState();
     const { addContact } = useContactsActions();
     const { retrieveChats } = usechatsActions();
 
     const props = defineProps<{
-        comment: SOCIAL_POST,
-        avatar: string
+        comment: SOCIAL_POST;
+        avatar: string;
     }>();
     const location = ref<string>();
+    const online = ref<boolean>();
     const router = useRouter();
 
     onBeforeMount(async () => {
         await retrieveChats();
         location.value = await myYggdrasilAddress();
+        const { isOnline } = await fetchStatus(props.comment.owner.id);
+        online.value = isOnline;
     });
 
     const isPersonFriend = computed(() => {
@@ -85,8 +98,7 @@
         return contacts.some(item => item.id === props.comment.owner.id);
     });
 
-
-    const goToChat = async (e) => {
+    const goToChat = async e => {
         e.preventDefault();
         if (location.value === props.comment.owner.location) return;
         if (!contacts.some(item => item.id === props.comment.owner.id)) {
@@ -99,16 +111,12 @@
         });
     };
 
-    const goToKutana = async (e) => {
+    const goToKutana = async e => {
         e.preventDefault;
         await nextTick(async () => {
             await router.push({ name: 'kutana' });
         });
     };
-
-
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
