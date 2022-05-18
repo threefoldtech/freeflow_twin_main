4aa94982e8d76b4ecb581f41cc388f7c6354060d
EdwardKerckhof <edward@zonedev.be>
2022-05-18 12:52:35 +0200

    Cleanup based on comments on PR

--- src/components/UserTable.vue
+++ src/components/UserTable.vue
@@ -38,14 +38,6 @@
                                 <td class="px-6 py-4 whitespace-nowrap w-full block">
                                     <div class="flex items-center justify-between">
                                         <div class="flex">
-                                            <!-- <div class="flex-shrink-0 h-10 w-10"> -->
-                                            <!--     <AvatarImg -->
-                                            <!--         :id="item.id" -->
-                                            <!--         :invitationList="true" -->
-                                            <!--         :showOnlineStatus="false" -->
-                                            <!--         alt="contact image" -->
-                                            <!--     /> -->
-                                            <!-- </div> -->
                                             <div class="ml-4">
                                                 <div class="text-sm font-medium text-gray-900">
                                                     {{ item.id }}
