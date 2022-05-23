a385734201dfaec8cc08b184c442187cdb8b9701
EdwardKerckhof <edward@zonedev.be>
2022-05-23 12:57:08 +0200

    docs(): add abstract repo JSDocs

--- apps/backend/src/nest/features/db/entity.repository.ts
+++ apps/backend/src/nest/features/db/entity.repository.ts
@@ -10,10 +10,26 @@ export abstract class EntityRepository<TEntity extends Entity> {
         this._entityRepository.createIndex();
     }
 
+    /**
+     * Finds all entities.
+     * @param {Object} obj - Object.
+     * @param {number} obj.offset - Paginated offset.
+     * @param {number} obj.count - Paginated count.
+     * @return {TEntity[]} - Found entities.
+     */
     async findAll({ offset, count }: { offset?: number; count?: number }): Promise<TEntity[]> {
         return await this._entityRepository.search().return.page(offset, count);
     }
 
+    /**
+     * Finds all entities based on search.
+     * @param {Object} obj - Object.
+     * @param {number} obj.offset - Paginated offset.
+     * @param {number} obj.count - Paginated count.
+     * @param {string} obj.where - Where entity field.
+     * @param {string} obj.eq - Equals.
+     * @return {TEntity[]} - Found entities.
+     */
     async findAllWhereEq({
         offset,
         count,
@@ -28,18 +44,39 @@ export abstract class EntityRepository<TEntity extends Entity> {
         return await this._entityRepository.search().where(where).eq(eq).return.page(offset, count);
     }
 
+    /**
+     * Finds one entity based on search.
+     * @param {Object} obj - Object.
+     * @param {string} obj.where - Where entity field.
+     * @param {string} obj.eq - Equals.
+     * @return {TEntity} - Found entity.
+     */
     async findOne({ where, eq }: { where: string; eq: string }): Promise<TEntity> {
         return await this._entityRepository.search().where(where).eq(eq).return.first();
     }
 
+    /**
+     * Creates a new entity in the database.
+     * @param {EntityCreationData} entity - Entity creation data.
+     * @return {TEntity} - Created entity.
+     */
     async save(entity: EntityCreationData): Promise<TEntity> {
         return await this._entityRepository.createAndSave(entity);
     }
 
-    async update(entity: EntityCreationData): Promise<TEntity> {
-        return await this._entityRepository.createAndSave(entity);
+    /**
+     * Updates an existing entity in the database.
+     * @param {TEntity} entity - Updated entity data.
+     * @return {TEntity} - Updated entity id.
+     */
+    async update(entity: TEntity): Promise<string> {
+        return await this._entityRepository.save(entity);
     }
 
+    /**
+     * Deletes an entity from the database.
+     * @param {string} entityId - Entity id to delete.
+     */
     async delete(entityId: string): Promise<void> {
         return await this._entityRepository.remove(entityId);
     }
