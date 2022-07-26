45f76ea8da51d060148b7b4dfb39379fb37d97c5
EdwardKerckhof <edward@zonedev.be>
2022-07-26 16:00:11 +0200

    fix(PWA): remove dev-dist folder

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -49,9 +49,6 @@ export default defineConfig({
                     },
                 ],
             },
-            devOptions: {
-                enabled: true,
-            },
         }),
     ],
     resolve: {
