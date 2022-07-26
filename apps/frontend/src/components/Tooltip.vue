20abb8950e17b43f5556a142d9cf904e190011c5
Baertie <44461137+Baertie@users.noreply.github.com>
2022-07-26 15:00:06 +0200

    =made a tooltip component and made icons bigger now when hovering the user will get the needed info

--- apps/frontend/src/components/Tooltip.vue
+++ apps/frontend/src/components/Tooltip.vue
@@ -4,11 +4,10 @@
             <div class="fas fa-question h-6 w-6 text-gray-500" aria-hidden="true"></div>
         </slot>
         <div
-            class="hidden inline-block absolute transition-opacity duration-500 invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700 w-80 ml-2 tablet:inset-x-0 tablet:top-4 tablet:ml-0"
+            class="inline-block absolute transition-opacity duration-500 invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700 w-max top-6"
         >
-            <div class="flex flex-col whitespace-pre-line my-2" v-for="textItem of text" :key="textItem">
-                <h5 class="text-primary text-center bg-white uppercase rounded p-1 mb-1">{{ textItem.title }}</h5>
-                <p class="text-white">{{ textItem.explanation }}</p>
+            <div class="flex flex-col my-2">
+                <p class="text-white">{{ textItem }}</p>
             </div>
         </div>
     </div>
@@ -16,7 +15,7 @@
 
 <script lang="ts" setup>
     interface IProps {
-        textItem: Array;
+        textItem: string;
     }
 
     const { textItem } = defineProps<IProps>();
