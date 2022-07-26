fa2af1cfef862bdd381432e44a423aa637676edf
EdwardKerckhof <edward@zonedev.be>
2022-07-26 18:54:26 +0200

    fix(config): use old way for now

--- apps/frontend/public/config/config.js
+++ apps/frontend/public/config/config.js
@@ -1,13 +1,14 @@
-const config = {
-    appBackend:
-        process.env.NODE_ENV !== 'production' ? 'https://login.staging.jimber.io/' : 'https://login.threefold.me/',
-    documentServerLocation:
-        process.env.NODE_ENV !== 'production'
-            ? 'https://documentserver.digitaltwin-test.jimbertesting.be/'
-            : 'https://documentserver.digitaltwin.jimbertesting.be/',
-    giphyApiKey: 'uk3XRSO0vYrPDEQKDPZJ2wGz33qzIxST',
-    beta: true,
-};
+// const config = {
+//     appBackend:
+//         process.env.NODE_ENV !== 'production' ? 'https://login.staging.jimber.io/' : 'https://login.threefold.me/',
+//     documentServerLocation:
+//         process.env.NODE_ENV !== 'production'
+//             ? 'https://documentserver.digitaltwin-test.jimbertesting.be/'
+//             : 'https://documentserver.digitaltwin.jimbertesting.be/',
+//     giphyApiKey: 'uk3XRSO0vYrPDEQKDPZJ2wGz33qzIxST',
+//     beta: true,
+// };
+import config from '/config/config.json' assert { type: 'json' };
 
 config.baseUrl = `${window.location.origin}/`;
 
