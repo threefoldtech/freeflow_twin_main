731c4ed2686325d4e5dc228537b856803527f59e
EdwardKerckhof <edward@zonedev.be>
2022-07-26 16:43:52 +0200

    fix(prettier): prettier

--- apps/frontend/src/components/ChatInput.vue
+++ apps/frontend/src/components/ChatInput.vue
@@ -211,7 +211,7 @@
     };
 
     if (props.chat.draft) {
-        console.log("CALLED")
+        console.log('CALLED');
         if (props.chat.draft?.action === 'EDIT') {
             messageInput.value = String(props.chat.draft.body);
             editMessage(props.chat.draft.to, props.chat.draft.body);
