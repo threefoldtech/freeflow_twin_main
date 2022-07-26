0fa552165f1f2d3dc298aa442a914b7e6aa4c72e
EdwardKerckhof <edward@zonedev.be>
2022-07-26 18:33:24 +0200

    fix(config): prettier

--- apps/frontend/public/config/config.js
+++ apps/frontend/public/config/config.js
@@ -1,6 +1,10 @@
 const config = {
-  appBackend: process.env.NODE_ENV !== 'production' ? 'https://login.staging.jimber.io/': 'https://login.threefold.me/',
-    documentServerLocation: process.env.NODE_ENV !== 'production' ? 'https://documentserver.digitaltwin-test.jimbertesting.be/': 'https://documentserver.digitaltwin.jimbertesting.be/',
+    appBackend:
+        process.env.NODE_ENV !== 'production' ? 'https://login.staging.jimber.io/' : 'https://login.threefold.me/',
+    documentServerLocation:
+        process.env.NODE_ENV !== 'production'
+            ? 'https://documentserver.digitaltwin-test.jimbertesting.be/'
+            : 'https://documentserver.digitaltwin.jimbertesting.be/',
     giphyApiKey: 'uk3XRSO0vYrPDEQKDPZJ2wGz33qzIxST',
     beta: true,
 };
