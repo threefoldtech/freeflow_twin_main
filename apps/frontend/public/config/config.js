b69bfc09f3d974fa04548fe712896e309d1f321d
EdwardKerckhof <edward@zonedev.be>
2022-07-26 19:03:04 +0200

    fix(config): use beta urls

--- apps/frontend/public/config/config.js
+++ apps/frontend/public/config/config.js
@@ -1,14 +1,9 @@
-// const config = {
-//     appBackend:
-//         process.env.NODE_ENV !== 'production' ? 'https://login.staging.jimber.io/' : 'https://login.threefold.me/',
-//     documentServerLocation:
-//         process.env.NODE_ENV !== 'production'
-//             ? 'https://documentserver.digitaltwin-test.jimbertesting.be/'
-//             : 'https://documentserver.digitaltwin.jimbertesting.be/',
-//     giphyApiKey: 'uk3XRSO0vYrPDEQKDPZJ2wGz33qzIxST',
-//     beta: true,
-// };
-import config from '/config/config.json' assert { type: 'json' };
+const config = {
+    appBackend: 'https://login.threefold.me/',
+    documentServerLocation: 'https://documentserver.digitaltwin.jimbertesting.be/',
+    giphyApiKey: 'uk3XRSO0vYrPDEQKDPZJ2wGz33qzIxST',
+    beta: true,
+};
 
 config.baseUrl = `${window.location.origin}/`;
 
