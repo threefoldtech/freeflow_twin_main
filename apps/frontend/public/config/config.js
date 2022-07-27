16a5d4b7d52468230ed3e275e85b7d3dd751019d
EdwardKerckhof <edward@zonedev.be>
2022-07-27 12:20:17 +0200

    fix(config): use javascript files instead of json

--- apps/frontend/public/config/config.js
+++ apps/frontend/public/config/config.js
@@ -1,9 +1,4 @@
-const config = {
-    appBackend: 'https://login.threefold.me/',
-    documentServerLocation: 'https://documentserver.digitaltwin.jimbertesting.be/',
-    giphyApiKey: 'uk3XRSO0vYrPDEQKDPZJ2wGz33qzIxST',
-    beta: true,
-};
+import config from '/config/config-def.js';
 
 config.baseUrl = `${window.location.origin}/`;
 
