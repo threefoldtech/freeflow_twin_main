568801afa1901e78616d594434f94299415ca301
JorisBassle <joris.bassle@gmail.com>
2022-08-01 12:39:23 +0200

    #92 video support in chat

---
+++ apps/frontend/src/components/MessageContentType/VideoContent.vue
@@ -0,0 +1,20 @@
+<template>
+    <video controls>
+        <source :src="src" />
+    </video>
+</template>
+
+<script lang="ts" setup>
+    import { calcExternalResourceLink } from '@/services/urlService';
+    import { Message, MessageBodyType } from '@/types';
+
+    interface IProp {
+        message: Message<MessageBodyType>;
+    }
+
+    const props = defineProps<IProp>();
+    const msgUrl = props.message.body.url;
+    const src = calcExternalResourceLink(msgUrl);
+</script>
+
+<style scoped></style>
