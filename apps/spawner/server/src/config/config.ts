42adb12fb2705be0802a20ac12c5db4e95468988
tobiaschielens <tobias.chielens@gmail.org>
2022-08-03 08:50:52 +0200

    Update staging url's

--- apps/spawner/server/src/config/config.ts
+++ apps/spawner/server/src/config/config.ts
@@ -1,7 +1,7 @@
 export const config = {
     appBackend: 'https://login.threefold.me',
     kycBackend: 'https://openkyc.live',
-    appId: 'beta.uhuru.me',
+    appId: 'demo.freeflow.life',
     seedPhrase:
         'calm science teach foil burst until next mango hole sponsor fold bottom cousin push focus track truly tornado turtle over tornado teach large fiscal',
 };
