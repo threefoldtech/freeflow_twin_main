9425c473c7452a0df18b0e8fb7a0f3ea3196e0d1
JorisBassle <joris.bassle@gmail.com>
2022-09-21 10:43:09 +0200

    #713 started with profile page

---
+++ apps/frontend/src/views/app/UserProfile.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <header>
+            <div>
+                <img src="" alt="cover photo" />
+            </div>
+            <div>
+                <AvatarImg :id="user.id" class="w-12 h-12" />
+                <h2>{{ user.name }}</h2>
+                <button>Edit profile</button>
+            </div>
+        </header>
+        <aside>
+            <!--    about me    -->
+            <!--     contact list     -->
+        </aside>
+        <main>
+            <!--    create post (if own profile)      -->
+            <!--    posts    -->
+        </main>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { useAuthState } from '@/store/authStore';
+    import AvatarImg from '@/components/AvatarImg.vue';
+
+    const { user } = useAuthState();
+</script>
+
+<style scoped></style>
