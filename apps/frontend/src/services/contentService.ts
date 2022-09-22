63357d8ad76fdb4f953d2b46c0a790ff24a7f0d1
JorisBassle <joris.bassle@gmail.com>
2022-09-22 11:21:14 +0200

    #744 use enum for file extensions

--- apps/frontend/src/services/contentService.ts
+++ apps/frontend/src/services/contentService.ts
@@ -1,80 +1,58 @@
+import {
+    IAudioFileExtension,
+    IImageFileExtension,
+    ISimpleFileExtension,
+    IVideoFileExtension,
+} from 'custom-types/file-actions.type';
+
 export const isImage = filename => {
     if (!filename) return false;
-    return (
-        filename.indexOf('.gif') !== -1 ||
-        filename.indexOf('.png') !== -1 ||
-        filename.indexOf('.jpg') !== -1 ||
-        filename.indexOf('.JPG') !== -1 ||
-        filename.indexOf('.jpeg') !== -1
-    );
+    return Object.values(IImageFileExtension).includes(filename.split('.').pop());
 };
 
 export const isAudio = filename => {
-    return filename?.indexOf('.mp3') !== -1 || filename.indexOf('.WebM') !== -1;
+    return Object.values(IAudioFileExtension).includes(filename.split('.').pop());
 };
 
 export const isSimpleTextFile = fileName => {
     if (!fileName) return false;
-    return (
-        fileName.indexOf('txt') !== -1 ||
-        fileName.indexOf('json') !== -1 ||
-        fileName.indexOf('html') !== -1 ||
-        fileName.indexOf('js') !== -1 ||
-        fileName.indexOf('ts') !== -1 ||
-        fileName.indexOf('css') !== -1 ||
-        fileName.indexOf('scss') !== -1 ||
-        fileName.indexOf('xml') !== -1 ||
-        fileName.indexOf('yml') !== -1 ||
-        fileName.indexOf('yaml') !== -1 ||
-        fileName.indexOf('md') !== -1 ||
-        fileName.indexOf('markdown') !== -1 ||
-        fileName.indexOf('python') !== -1
-    );
-};
-
-export const extensionToLanguage = (extension: string) => {
-    switch (extension) {
-        case 'js':
-            return 'javascript';
-        case 'ts':
-            return 'typescript';
-        case 'html':
-            return 'html';
-        case 'css':
-            return 'css';
-        case 'scss':
-            return 'scss';
-        case 'yml':
-            return 'yaml';
-        case 'yaml':
-            return 'yaml';
-        case 'md':
-            return 'markdown';
-        case 'markdown':
-            return 'markdown';
-        case 'python':
-            return 'python';
-        default:
-            return 'text';
-    }
+    return Object.values(ISimpleFileExtension).includes(fileName.split('.').pop());
 };
 
 export const isVideo = filename => {
     if (!filename) return false;
-    return (
-        filename?.indexOf('.mp4') !== -1 ||
-        filename?.indexOf('.mov') !== -1 ||
-        filename?.indexOf('.avi') !== -1 ||
-        filename?.indexOf('.mkv') !== -1 ||
-        filename?.indexOf('.flv') !== -1 ||
-        filename?.indexOf('.wmv') !== -1 ||
-        filename?.indexOf('.mpg') !== -1 ||
-        filename?.indexOf('.mpeg') !== -1 ||
-        filename?.indexOf('.m4v') !== -1 ||
-        filename?.indexOf('.3gp') !== -1 ||
-        filename?.indexOf('.3g2') !== -1 ||
-        filename?.indexOf('.m2ts') !== -1 ||
-        filename?.indexOf('.mts') !== -1 ||
-        filename?.indexOf('.mpe') !== -1
-    );
+    return Object.values(IVideoFileExtension).includes(filename.split('.').pop());
+};
+
+export const extensionToLanguage = (extension: string) => {
+    switch (extension) {
+        case ISimpleFileExtension.TXT:
+            return 'javascript';
+        case ISimpleFileExtension.JSON:
+            return 'json';
+        case ISimpleFileExtension.HTML:
+            return 'html';
+        case ISimpleFileExtension.JS:
+            return 'javascript';
+        case ISimpleFileExtension.TS:
+            return 'typescript';
+        case ISimpleFileExtension.CSS:
+            return 'css';
+        case ISimpleFileExtension.SCSS:
+            return 'scss';
+        case ISimpleFileExtension.XML:
+            return 'xml';
+        case ISimpleFileExtension.YML:
+            return 'yaml';
+        case ISimpleFileExtension.YAML:
+            return 'yaml';
+        case ISimpleFileExtension.MD:
+            return 'markdown';
+        case ISimpleFileExtension.MARKDOWN:
+            return 'markdown';
+        case ISimpleFileExtension.PYTHON:
+            return 'python';
+        default:
+            return 'text';
+    }
 };
