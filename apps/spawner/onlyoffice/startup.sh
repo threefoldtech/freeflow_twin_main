206108eca6b341cd8dccd553632580e3f8e24139
LennertDefauw <lennert.defauw@jimber.org>
2022-11-12 20:02:12 +0100

    onlyoffice undo jwt

--- apps/spawner/onlyoffice/startup.sh
+++ apps/spawner/onlyoffice/startup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 yggdrasil -useconffile /etc/yggdrasil.conf &
-/app/ds/run-document-server.sh
+
+/app/ds/run-document-server.sh &
+sleep 5s
+
+sed -i 's/\"inbox\": true/\"inbox\":false/' /etc/onlyoffice/documentserver/local.json
+sed -i 's/\"outbox\": true/\"outbox\":false/' /etc/onlyoffice/documentserver/local.json
+sed -i 's/\"browser\": true/\"browser\":false/' /etc/onlyoffice/documentserver/local.json
+
+
+tail -f /dev/null
