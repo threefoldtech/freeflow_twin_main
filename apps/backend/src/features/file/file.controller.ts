bb4756b1d69bf2729d0acf63d3635c1db62a0942
LennertDefauw <lennert.defauw@jimber.org>
2023-01-26 14:57:21 +0100

    use old file hanlder

--- apps/backend/src/features/file/file.controller.ts
+++ apps/backend/src/features/file/file.controller.ts
@@ -42,9 +42,10 @@ export class FileController {
         if (!path || !this._fileService.exists({ path: filePath }))
             throw new BadRequestException('please provide a valid file id');
 
-        // When there is a wav file, different handling of file return since fileStream is not working on Safari
         if (path.endsWith('.wav')) {
-            return res.sendFile(filePath);
+            req.res.setHeader(`content-type`, `audio/wav`);
+            req.res.setHeader(`content-transfer-encoding`, 'binary');
+            req.res.setHeader(`Accept-Ranges`, 'bytes');
         }
 
         const fileBuffer = this._fileService.readFile({ path: filePath });
