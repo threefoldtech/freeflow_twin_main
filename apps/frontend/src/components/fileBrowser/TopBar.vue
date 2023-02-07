d58ca9e4ef796ed6ebfa613997c81a7496142ddf
JorisBassle <joris.bassle@gmail.com>
2023-02-07 18:08:18 +0100

    #982 possible fix

--- apps/frontend/src/components/fileBrowser/TopBar.vue
+++ apps/frontend/src/components/fileBrowser/TopBar.vue
@@ -10,7 +10,12 @@
                     class="focus:ring-primary focus:border-primary block w-full pl-10 sm:text-sm border-gray-300 rounded-md"
                     placeholder="Search"
                     type="text"
-                    @input="debounceSearch"
+                    @input="
+                        e => {
+                            searchDirValue = e.target.value;
+                            debounceSearch();
+                        }
+                    "
                 />
                 <div
                     v-if="searchDirValue !== ''"
