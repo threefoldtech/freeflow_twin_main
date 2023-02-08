c2fbc401f221d2535732687fa2e39622eff0cae8
JorisBassle <joris.bassle@gmail.com>
2023-02-08 20:37:28 +0100

    #992 fix

--- apps/frontend/src/components/UserTableGroup.vue
+++ apps/frontend/src/components/UserTableGroup.vue
@@ -7,7 +7,12 @@
             type="text"
             v-model="searchTerm"
             @focus="handleInput"
-            @input="handleInput"
+            @input="
+                e => {
+                    searchTerm = e.target.value;
+                    handleInput;
+                }
+            "
             class="focus:ring-primary focus:border-primary block w-full pl-10 sm:text-sm border-gray-300 rounded-md"
             :placeholder="placeholder"
             :v-focus="focus"
