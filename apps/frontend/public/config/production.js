b1f2ef761d3372d6d280fb1627cbeb5cf4a94508
Lennert Defauw <lennert.defauw@jimber.org>
2023-06-13 20:54:43 +0200

    fix: document server URL

--- apps/frontend/public/config/production.js
+++ apps/frontend/public/config/production.js
@@ -1,7 +1,7 @@
 export default {
     spawnerUrl: 'demo.freeflow.life',
     appBackend: 'https://login.threefold.me/',
-    documentServerLocation: 'https://documentserver.digitaltwin.jimbertesting.be/',
+    documentServerLocation: 'https://documentserver.demo.freeflow.life/',
     giphyApiKey: 'uk3XRSO0vYrPDEQKDPZJ2wGz33qzIxST',
     beta: true,
 };
